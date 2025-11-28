c17232ee790662d897eaa209442c911c12a235c5
ikawamuk <ikawamuk@student.42.fr>
2025-11-28 17:33:16 +0900

    update: add question comment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 16:03:37 by khanadat          #+#    #+#             */
-/*   Updated: 2025/11/28 17:15:20 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/11/28 17:33:13 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,10 @@ int	main(int argc, char *argv[])
 	free(content);
 	return (MINIRT_SUCCESS);
 }
+/*
+・ARG_NUMはむしろマジックナンバー「2」のほうが可読性たかいのでは？
+・MINIRT_FAILUREよりはEXIT_FAIRUE, EXIT_SUCCESSの標準マクロを使ったほうがいい気がする。
+・拡張子はfile extensionなのでRT_EXTENTIONとか？
+・int minirt_strrncmp(const char *s1, const char *s2, size_t n)はft_strncmpじゃだめか？
+・set_contentはreadよりget_next_line()使って良くはないか？
+*/
