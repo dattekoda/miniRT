e7511f7f4c3d8d10c7dafdadf234db238f21218f
khanadat <khanadat@student.42tokyo.jp>
2026-01-09 22:05:27 +0900

    update validate

--- src/init_world/validate/validate.c
+++ src/init_world/validate/validate.c
@@ -6,7 +6,7 @@
 /*   By: khanadat <khanadat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 18:39:54 by khanadat          #+#    #+#             */
-/*   Updated: 2026/01/09 21:22:39 by khanadat         ###   ########.fr       */
+/*   Updated: 2026/01/09 22:05:08 by khanadat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,35 @@ int			validate_camera(char *line);
 int			validate_light(char *line);
 int			validate_sphere(char *line);
 
+int			validate_line(char *line);
 static int	match_identifier(const char *line, const char *id, size_t size);
 static int	validate_invalid_id(char *line);
+
+int	validate_line_list(const t_list *line_list)
+{
+	int		ret_value;
+	size_t	ambient_count;
+	size_t	camera_count;
+
+	ret_value = 0;
+	ambient_count = 0;
+	camera_count = 0;
+	while (line_list)
+	{
+		if (match_identifier(line_list->content, "A", 1))
+			ret_value = (ambient_count++, 1);
+		if (match_identifier(line_list->content, "C", 1))
+			ret_value = (camera_count++, 1);
+		if (validate_line(line_list->content))
+			ret_value = 1;
+		line_list = line_list->next;
+	}
+	if (ambient_count != 1)
+		ft_putendl_fd("set only one ambient", STDERR_FILENO);
+	if (camera_count != 1)
+		ft_putendl_fd("set only one camera", STDERR_FILENO);
+	return (ret_value);
+}
 
 int	validate_line(char *line)
 {
