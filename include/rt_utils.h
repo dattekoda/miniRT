d7c02d758f3214804e2026ee070de7327f3a40a5
ikawamuk <ikawamuk@student.42.fr>
2026-01-10 01:26:46 +0900

    update: norm

--- include/rt_utils.h
+++ include/rt_utils.h
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 01:14:17 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/01/10 01:14:52 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/01/10 01:26:36 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define RT_UTILS_H
 
 # include "rt_define.h"
-# include <stdio.h> // delete later
+# include <stdio.h> // delete later 
 
 void	err_rt(void);
 void	perror_rt(const char *syscall_fname);
