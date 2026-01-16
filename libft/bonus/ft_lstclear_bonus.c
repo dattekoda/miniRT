84db7dfa5a228ad259488df71075335032616631
ikawamuk <ikawamuk@student.42.fr>
2026-01-16 18:37:08 +0900

    update: sorry

--- libft/bonus/ft_lstclear_bonus.c
+++ libft/bonus/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:25:23 by khanadat          #+#    #+#             */
-/*   Updated: 2026/01/16 18:35:28 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/01/16 18:37:03 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	if (!lst)
-		return ;
 	while (*lst)
 	{
 		tmp = (*lst)->next;
