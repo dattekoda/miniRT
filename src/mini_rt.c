10e5669fe9f550b6b459ffb1fff970443000f5d9
ikawamuk <ikawamuk@student.42.fr>
2026-01-28 20:32:03 +0900

    update:

--- src/mini_rt.c
+++ src/mini_rt.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 19:00:17 by khanadat          #+#    #+#             */
-/*   Updated: 2026/01/28 20:08:29 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/01/28 20:32:01 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int		init_world(t_world *world, int *option_flag, int argc, char *argv[]);
 void	clear_world(t_world world);
-int		paint_pixel(t_color **pxl_arr_p, const t_world *world);
+int		render_pixel(t_color **pxl_arr_p, const t_world *world);
 int		draw_image(const t_color *pixel_arr, int option_flag, void **mlx_p);
 
 int	mini_rt(int argc, char *argv[])
@@ -31,7 +31,7 @@ int	mini_rt(int argc, char *argv[])
 	if (init_world(&world, &option_flag, argc, argv) == FAILURE)
 		return (EXIT_FAILURE);
 	pixel_arr = NULL;
-	if (paint_pixel(&pixel_arr, &world) == FAILURE)
+	if (render_pixel(&pixel_arr, &world) == FAILURE)
 	{
 		clear_world(world);
 		return (EXIT_FAILURE);
