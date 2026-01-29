35f374cc0c884c8dc37969f2018cb58041860f68
ikawamuk <ikawamuk@student.42.fr>
2026-01-29 20:53:49 +0900

    update: divide and concer

--- src/init_world/create_world/hitter/line_to_hitter/line_to_light.c
+++ src/init_world/create_world/hitter/line_to_hitter/line_to_light.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 14:14:44 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/01/29 20:27:30 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/01/29 20:53:27 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,23 @@
 #include "rt_config.h"
 #include "light.h"
 
-static int	light_line_to_shape_param(
-				const char *line,
-				t_sphere *light_param,
-				int option_flag);
-int			line_to_material(
-				const char *line,
-				size_t *line_idx,
-				t_material **mat_pp,
-				const t_element *element);
-static int	line_to_light_material(
-				const char *line,
-				size_t *line_idx,
-				t_material **mat_pp,
-				int option_flag);
-static int	param_to_light_material(t_color color, t_material **mat_pp);
+static int		light_line_to_shape_param(
+					const char *line,
+					t_sphere *light_param,
+					int option_flag);
+int				line_to_material(
+					const char *line,
+					size_t *line_idx,
+					t_material **mat_pp,
+					const t_element *element);
+static int		line_to_light_material(
+					const char *line,
+					size_t *line_idx,
+					t_material **mat_pp,
+					int option_flag);
+static int		param_to_light_material(
+					t_color color, t_material **mat_pp);
+static t_color	calc_color(t_color raw_color, double ratio, int option_flag);
 
 /*
 @brief line "L 0,4,4, 0.3, 255,255,0 4"
@@ -59,33 +61,34 @@ static int	light_line_to_shape_param(
 				int option_flag)
 {
 	size_t	i;
+	double	ratio;
+	t_color	raw_color;
 
 	i = g_info_table[LIGHT]->id_len;
 	token_to_vec(line, &i, &light_param->center);
-	if (line_to_light_material(
-			line, &i, &light_param->hitter.mat_ptr, option_flag) == FAILURE)
+	token_to_value(line, &i, &ratio);
+	token_to_vec(line, &i, &raw_color);
+	if (param_to_light_material(
+			calc_color(raw_color, ratio, option_flag),
+			&light_param->hitter.mat_ptr) == FAILURE)
 		return (FAILURE);
 	token_to_value(line, &i, &light_param->radius);
 	return (SUCCESS);
 }
 
-static int	line_to_light_material(const char *line, size_t *line_idx,
-	t_material **mat_pp, int option_flag)
+static t_color	calc_color(t_color raw_color, double ratio, int option_flag)
 {
-	double		ratio;
-	t_color		color;
+	t_color	color;
 
-	token_to_value(line, line_idx, &ratio);
 	if (!(option_flag & OPT_ARTIFICIAL))
 		ratio *= PATHTRACING_LIGHT_STRENGTH;
-	token_to_vec(line, line_idx, &color);
-	color = scal_mul_vec3(normalize_color(color), ratio);
-	if (param_to_light_material(color, mat_pp) == FAILURE)
-		return (FAILURE);
-	return (SUCCESS);
+	color = scal_mul_vec3(normalize_color(raw_color), ratio);
+	return (color);
 }
 
-static int	param_to_light_material(t_color color, t_material **mat_pp)
+static int	param_to_light_material(
+				t_color color,
+				t_material **mat_pp)
 {
 	t_texture	*texture_ptr;
 	t_material	*mat_ptr;
