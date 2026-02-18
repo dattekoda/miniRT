31bd607cd66fbd469e77d453a861d9fc4b8db18d
ikawamuk <ikawamuk@student.42.fr>
2026-02-18 21:30:30 +0900

    update:

--- src/mini_rt.c
+++ src/mini_rt.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 19:00:17 by khanadat          #+#    #+#             */
-/*   Updated: 2026/02/18 21:42:09 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/02/18 21:43:28 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,6 @@ int	mini_rt(int argc, char *argv[])
 	option_flag = set_option(argv + 2);
 	if (init_world(&world, argv[1], option_flag) == FAILURE)
 		return (FAILURE);
-	printf("\nFinish init_world()\n"); // debug
-	exit(0); // debug
 	if (render_world(&world, option_flag) == FAILURE)
 		return (FAILURE);
 	return (SUCCESS);
