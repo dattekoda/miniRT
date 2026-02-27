236d972804d1839c19d0e28f67ad23878f20d288
ikawamuk <ikawamuk@student.42.fr>
2026-02-28 01:41:32 +0900

    update:

--- src/init_world/define/define_element.c
+++ src/init_world/define/define_element.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 13:12:12 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/02/28 01:27:10 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/02/28 01:36:23 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	line_to_plane(t_hitter **plane, const char *line);
 int	line_to_disk(t_hitter **disk, const char *line);
 int	line_to_cylinder(t_hitter **cylinder, const char *line);
 int	line_to_triangle(t_hitter **triangle, const char *line);
+int	line_to_cone(t_hitter **cone, const char *line);
 
 extern const t_skip					g_ambient_skips[];
 extern const t_skip					g_camera_skips[];
@@ -112,7 +113,7 @@ const t_element						g_cone_info = {
 	.input_format = "co [coordinate](x,y,z) [normalized orient vector](0-1) \
 [angle](0-90) [R,G,B](0-255)",
 	.skip_arr = g_cone_skips,
-	.line_to_hitter = NULL,
+	.line_to_hitter = line_to_cone,
 	.material_idx = CONE_MATERIAL_ID,
 	.texture_idx = CONE_TEXTURE_ID
 };
@@ -146,7 +147,7 @@ const t_element						*g_element_table[] = {
 
 const t_element						*g_infinite_table[] = {
 	&g_plane_info,
-	// &g_cone_info,
+	&g_cone_info,
 	NULL
 };
 
