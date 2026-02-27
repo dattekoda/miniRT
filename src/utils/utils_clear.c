847471d94c3bf3003ec32362c2b10ac9f1e99036
ikawamuk <ikawamuk@student.42.fr>
2026-02-28 01:32:28 +0900

    update: double free of worlds obj tree

--- src/utils/utils_clear.c
+++ src/utils/utils_clear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_clear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khanadat <khanadat@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 12:39:52 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/02/27 23:24:35 by khanadat         ###   ########.fr       */
+/*   Updated: 2026/02/28 01:31:58 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,6 @@ void	clear_world(t_world *world)
 	if (world->object_tree)
 	{
 		world->object_tree->clear(world->object_tree);
-		free(world->object_tree);
+		// free(world->object_tree);
 	}
 }
