24730506da50d145bb7fc74478b2bb3742d8ad56
khanadat <khanadat@student.42tokyo.jp>
2026-02-28 16:16:00 +0900

    add null check

--- src/init_world/create_world/set_objects/line_list_to_hitter_arr/add_cylinder_disk.c
+++ src/init_world/create_world/set_objects/line_list_to_hitter_arr/add_cylinder_disk.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   add_cylinder_disk.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: khanadat <khanadat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 22:12:55 by khanadat          #+#    #+#             */
-/*   Updated: 2026/02/28 00:29:45 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/02/28 16:15:20 by khanadat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 #include "vec_utils.h"
 #include <stdlib.h>
 
-static t_disk	cylinder_to_upper_disk(t_cylinder cylinder);
-static t_disk	cylinder_to_lower_disk(t_cylinder cylinder);
+static int		cylinder_to_lower_disk(t_cylinder cylinder, t_disk *lower_disk);
+static int		cylinder_to_upper_disk(t_cylinder cylinder, t_disk *upper_disk);
 static int		add_disk(t_list **hitter_list, t_disk disk);
 
 int	add_cylinder_disk(t_list **hitter_list, t_cylinder *cylinder)
@@ -26,10 +26,12 @@ int	add_cylinder_disk(t_list **hitter_list, t_cylinder *cylinder)
 	t_disk		upper_disk;
 	t_disk		lower_disk;
 
-	upper_disk = cylinder_to_upper_disk(*cylinder);
+	if (cylinder_to_upper_disk(*cylinder, &upper_disk) == FAILURE)
+		return (FAILURE);
 	if (add_disk(hitter_list, upper_disk) == FAILURE)
 		return (FAILURE);
-	lower_disk = cylinder_to_lower_disk(*cylinder);
+	if (cylinder_to_lower_disk(*cylinder, &lower_disk) == FAILURE)
+		return (FAILURE);
 	if (add_disk(hitter_list, lower_disk) == FAILURE)
 		return (FAILURE);
 	return (SUCCESS);
@@ -50,30 +52,29 @@ static int	add_disk(t_list **hitter_list, t_disk disk_param)
 	return (SUCCESS);
 }
 
-#include <stdio.h>
-static t_disk	cylinder_to_lower_disk(t_cylinder cylinder)
+static int	cylinder_to_upper_disk(t_cylinder cylinder, t_disk *upper_disk)
 {
-	t_disk		disk_param;
-
-	ft_bzero(&disk_param, sizeof(t_disk));
-	disk_param.center = cylinder.center;
-	disk_param.normal = negative_vec3(cylinder.direct);
-	disk_param.radius = cylinder.radius;
-	disk_param.hitter.mat_ptr = cylinder.hitter.mat_ptr->clone(cylinder.hitter.mat_ptr);
-	// NEED null check
-	return (disk_param);
+	ft_bzero(upper_disk, sizeof(t_disk));
+	upper_disk->center = add_vec3(cylinder.center, scal_mul_vec3(cylinder.direct,
+				cylinder.height));
+	upper_disk->normal = cylinder.direct;
+	upper_disk->radius = cylinder.radius;
+	upper_disk->hitter.mat_ptr
+		= cylinder.hitter.mat_ptr->clone(cylinder.hitter.mat_ptr);
+	if (!upper_disk->hitter.mat_ptr)
+		return (FAILURE);
+	return (SUCCESS);
 }
 
-static t_disk	cylinder_to_upper_disk(t_cylinder cylinder)
+static int	cylinder_to_lower_disk(t_cylinder cylinder, t_disk *lower_disk)
 {
-	t_disk		disk_param;
-
-	ft_bzero(&disk_param, sizeof(t_disk));
-	disk_param.center = add_vec3(cylinder.center, scal_mul_vec3(cylinder.direct,
-				cylinder.height));
-	disk_param.normal = cylinder.direct;
-	disk_param.radius = cylinder.radius;
-	disk_param.hitter.mat_ptr = cylinder.hitter.mat_ptr->clone(cylinder.hitter.mat_ptr);
-	// NEED null check
-	return (disk_param);
+	ft_bzero(lower_disk, sizeof(t_disk));
+	lower_disk->center = cylinder.center;
+	lower_disk->normal = negative_vec3(cylinder.direct);
+	lower_disk->radius = cylinder.radius;
+	lower_disk->hitter.mat_ptr
+		= lower_disk->hitter.mat_ptr->clone(lower_disk->hitter.mat_ptr);
+	if (!lower_disk->hitter.mat_ptr)
+		return (FAILURE);
+	return (SUCCESS);
 }
