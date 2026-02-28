83b99f4fa13134ddcdc225dd180ea20d652e39b3
khanadat <khanadat@student.42tokyo.jp>
2026-02-28 17:17:18 +0900

    delete debug msg

--- src/mini_rt.c
+++ src/mini_rt.c
@@ -6,7 +6,7 @@
 /*   By: khanadat <khanadat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 19:00:17 by khanadat          #+#    #+#             */
-/*   Updated: 2026/02/28 16:48:00 by khanadat         ###   ########.fr       */
+/*   Updated: 2026/02/28 17:16:52 by khanadat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ int	mini_rt(int argc, char *argv[])
 	option_flag = set_option(argv + 2);
 	if (init_world(&world, argv[1], option_flag) == FAILURE)
 		return (FAILURE);
-	print_tree(world.object_tree);
 	if (render_world(&world, option_flag) == FAILURE)
 		return (FAILURE);
 	return (SUCCESS);
